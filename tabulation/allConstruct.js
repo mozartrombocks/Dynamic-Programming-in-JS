5985a5d396d0d239845f661b6eb81cfef1b79140
mozartrombocks <shadman9702@gmail.com>
2021-01-12 15:24:06 -0500

    Initial Commit

---
+++ tabulation/allConstruct.js
@@ -0,0 +1,39 @@
+const allConstruct = (target, wordBank) => {
+    const table = Array(target.length + 1)
+    .fill()
+    .map(() => []);
+
+table[0] = [[]];
+
+for (let i = 0; i <= target.length; i++){
+    for (let word of wordBank) {
+        if (target.slice(i, i + word.length) === word){
+            const newCombination = table[i].map(subArray => [ ...subArray, word ]);
+            table[i + word.length].push(...newCombination) ;
+
+        }
+    }
+}
+
+return table[target.length];
+};
+
+console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
+// [
+//  ['purp', 'le'],
+//  ['p', 'ur', 'p', 'le']
+// ]     
+
+console.log(allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd', 'ef', 'c']));
+// [
+//   ['ab', 'cd', 'ef'],
+//   ['ab', 'c', 'def'],
+//   ['abc', 'def'],
+//   ['abcd', 'ef']
+// ]
+
+console.log(allConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
+// []
+
+console.log(allConstruct('aaaaaaaaz', ['a', 'aa', 'aaa', 'aaaa', 'aaaaa']));
+// []
